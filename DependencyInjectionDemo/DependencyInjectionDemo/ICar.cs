afc1d72558c163f146bc13ec070e33cff5eb66ea
wangfeijian <wangfeijianhao@163.com>
2021-03-22 13:49:05 +0800

    增加依赖注入Demo程序

---
+++ DependencyInjectionDemo/DependencyInjectionDemo/ICar.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionDemo
+{
+    public interface ICar
+    {
+        string Name { get; set; }
+        void Run();
+    }
+
+    public class Car : ICar
+    {
+        public string Name { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+        }
+        public void Run()
+        {
+            Console.WriteLine($"{Name} Run Fast");
+        }
+    }
+
+    public class Bus : ICar
+    {
+        public Bus(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public void Run()
+        {
+            Console.WriteLine($"{Name} Run slowly");
+        }
+    }
+
+    public interface IDriver
+    {
+        void Driver();
+    }
+
+    public class OldDriver : IDriver
+    {
+        private ICar car;
+        public OldDriver(ICar car)
+        {
+            this.car = car;
+        }
+        public void Driver()
+        {
+            Console.WriteLine($"老司机开{car.Name}");
+            car.Run();
+            Console.WriteLine("老司机车开的很好");
+        }
+    }
+
+    public class WomanDriver : IDriver
+    {
+        private ICar car;
+        public WomanDriver(ICar car)
+        {
+            this.car = car;
+        }
+        public void Driver()
+        {
+            Console.WriteLine($"女司机开{car.Name}");
+            car.Run();
+            Console.WriteLine("女司机开车技术不好");
+        }
+    }
+}
---
+++ DependencyInjectionDemo/DependencyInjectionDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DependencyInjectionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 准备一个服务容器
+            IServiceCollection collection = new ServiceCollection();
+
+            // 向服务容器中添加所需要的服务
+            collection.AddSingleton("mini");
+            collection.AddSingleton<ICar, Car>();
+            collection.AddSingleton<IDriver, OldDriver>();
+
+            //collection.AddScoped<ICar, Car>(x => new Car("mini"));
+            //collection.AddScoped<IDriver, WomanDriver>();
+
+            // 注册服务
+            ServiceProvider service = collection.BuildServiceProvider();
+
+            // 获取服务
+            var driver = service.GetRequiredService<IDriver>();
+            driver.Driver();
+
+        }
+    }
+}
