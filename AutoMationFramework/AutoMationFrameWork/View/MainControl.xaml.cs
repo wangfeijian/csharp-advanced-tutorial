8eab44e036cefc19bf53952c35736a26404442a6
wangfeijian <wangfeijianhao@163.com>
2021-06-28 11:10:18 +0800

    更新Cpu及内存显示

--- AutoMationFramework/AutoMationFrameWork/View/MainControl.xaml.cs
+++ AutoMationFramework/AutoMationFrameWork/View/MainControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AutoMationFrameWork.View
 {
@@ -20,9 +22,82 @@ namespace AutoMationFrameWork.View
     /// </summary>
     public partial class MainControl : UserControl
     {
+        private PerformanceCounter m_CpuCounter;
+        private PerformanceCounter m_RamCounter;
+        private PerformanceCounter m_GcCounter;
+        private string CpuStr;
+        private string RamStr;
+        private string Ram2Str;
+
         public MainControl()
         {
             InitializeComponent();
+
+            string strProcessName = Process.GetCurrentProcess().ProcessName;
+            m_CpuCounter = new PerformanceCounter("Process", "% Processor Time", strProcessName);
+            m_RamCounter = new PerformanceCounter("Process", "Working Set - Private", strProcessName);
+            m_GcCounter = new PerformanceCounter(".NET CLR Memory", "% Time in GC", strProcessName);
+
+            var showTimer = new DispatcherTimer();
+            showTimer.Tick += ShowTime;
+            showTimer.Interval = new TimeSpan(0, 0, 0, 1);
+            showTimer.Start();
+        }
+
+        private void ShowTime(object sender, EventArgs e)
+        {
+            double cpu = m_CpuCounter.NextValue();
+            double ram = m_RamCounter.NextValue();
+            double gc = m_GcCounter.NextValue();
+            string level = "B";
+
+            ByteConvert(ref ram, ref level);
+
+            CpuStr = cpu.ToString("F1") + "%";
+            RamStr = $"{ram:F1}";
+            Ram2Str = level;
+
+            CpuProgressBar.Value = cpu;
+            RamProgressBar.Value = gc;
+            CpuTextBlock.Text = CpuStr;
+            RamTextBlock.Text = RamStr;
+            Ram2TextBlock.Text = Ram2Str;
+
+            string strTime = DateTime.Now.ToString("yyyy-MM-dd dddd hh:mm:ss");
+            TimeTextBlock.Text = strTime;
+        }
+
+        private void ByteConvert(ref double v, ref string level)
+        {
+            switch (level)
+            {
+                case "B":
+                    if (v / 1024 > 1)
+                    {
+                        level = "KB";
+                        v /= 1024;
+                        ByteConvert(ref v, ref level);
+                    }
+                    break;
+
+                case "KB":
+                    if (v / 1024 > 1)
+                    {
+                        level = "MB";
+                        v /= 1024;
+                        ByteConvert(ref v, ref level);
+                    }
+                    break;
+
+                case "MB":
+                    if (v / 1024 > 1)
+                    {
+                        level = "GB";
+                        v /= 1024;
+                        ByteConvert(ref v, ref level);
+                    }
+                    break;
+            }
         }
     }
 }
