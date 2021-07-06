e7e1b0a8604ee12ba14fc81ba819e68fce8979a5
wangfeijian <wangfeijianhao@163.com>
2021-07-06 11:31:26 +0800

    添加halcon窗体，并将背景颜色改成指定颜色

---
+++ AutoMationFramework/AutoMationFrameWork/View/ImageControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HalconDotNet;
+
+namespace AutoMationFrameWork.View
+{
+    /// <summary>
+    /// ImageControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageControl : UserControl
+    {
+        public ImageControl()
+        {
+            InitializeComponent();
+        }
+
+        private void SetHalconWindow()
+        {
+            HWindow h = HSmartWindow.HalconWindow;
+            if (h != null)
+            {
+                HOperatorSet.SetWindowParam(h,"background_color", "#50bc93");
+                HOperatorSet.ClearWindow(h);
+            }
+        }
+
+        private void HSmartWindow_HInitWindow(object sender, EventArgs e)
+        {
+            SetHalconWindow();
+        }
+
+    }
+}
