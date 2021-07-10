7b7781f750ee66d47aab02f2580353be40795334
wangfeijian <wangfeijianhao@163.com>
2021-07-10 13:22:43 +0800

    增加视觉界面侧边手动控制，增加显示或隐藏侧边栏按钮

--- AutoMationFramework/AutoMationFrameWork/View/CameraControl.xaml.cs
+++ AutoMationFramework/AutoMationFrameWork/View/CameraControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
 
 namespace AutoMationFrameWork.View
 {
@@ -23,6 +24,22 @@ namespace AutoMationFrameWork.View
         public CameraControl()
         {
             InitializeComponent();
+        }
+
+        private void HideButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            if (HideBorder.Width<=0)
+            {
+                HideBorder.Width = 300;
+                HideButton.Margin = new Thickness(-10,10,0,0);
+                HideButtonIcon.Kind = PackIconKind.ChevronRightBoxOutline;
+            }
+            else
+            {
+                HideBorder.Width = 0;
+                HideButton.Margin = new Thickness(-25, 10, 0, 0);
+                HideButtonIcon.Kind = PackIconKind.ChevronLeftBoxOutline;
+            }
         }
     }
 }
