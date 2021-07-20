7d4cf879a58065a238531d36e0ccccbb0d22f75e
王飞箭 <wangfeijianhao@163.com>
2021-07-20 19:26:29 +0800

    添加语言配置文件，增加一个程序运行只能运行一次互斥锁。

--- AutoMationFramework/AutoMationFrameWork/App.xaml.cs
+++ AutoMationFramework/AutoMationFrameWork/App.xaml.cs
@@ -1,17 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
+using System.Windows.Threading;
+using CommonTools;
+using Newtonsoft.Json;
 
 namespace AutoMationFrameWork
 {
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
+        private static DispatcherOperationCallback exitFrameCallback = ExitFrame;
+        private Mutex mutex;
+
+        [DllImport("Kernel32.dll")]
+        public static extern bool SetDllDirectory(string lpPathName);
+
+        public App()
+        {
+            Startup += (StartupEventHandler)((s, e) =>
+           {
+               bool createdNew;
+               mutex = new Mutex(true, "AutoMationFrameWork", out createdNew);
+               ResourceDictionary resource = LocationServices.GetResource(new CultureInfo(GetLangType()));
+               if (resource != null)
+               {
+                   Resources.MergedDictionaries.Add(resource);
+                   Thread.CurrentThread.CurrentUICulture = new CultureInfo(GetLangType());
+               }
+               else
+               {
+                   LocationServices.GetResource(new CultureInfo("zh-CN".ToString()));
+                   Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN".ToString());
+               }
+               if (!createdNew)
+               {
+                   MessageBox.Show(LocationServices.GetLang("Process Exists"));
+                   Environment.Exit(0);
+               }
+           });
+            var directoryInfo = new DirectoryInfo(System.Windows.Forms.Application.ExecutablePath).Parent;
+            if (directoryInfo != null)
+                SetDllDirectory(directoryInfo.FullName + "\\PublicFile\\x64");
+            FrameworkCompatibilityPreferences.AreInactiveSelectionHighlightBrushKeysSupported = false;
+        }
+
+
+        private string GetLangType()
+        {
+            string str = "zh_cn";
+            try
+            {
+                string filename = Directory.GetCurrentDirectory() + "\\Config\\config.json";
+                var reslut = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(filename));
+                return reslut["lang"];
+            }
+            catch (Exception)
+            {
+                return str;
+            }
+        }
+
+        private static object ExitFrame(object state)
+        {
+            ((DispatcherFrame) state).Continue = false;
+            return null;
+        }
     }
 }
---
+++ AutoMationFramework/CommonTools/LocationServices.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommonTools
+{
+    public static class LocationServices
+    {
+        public static ResourceDictionary GetResource(CultureInfo cultureInfo)
+        {
+            Uri uri = new Uri(Directory.GetCurrentDirectory()+"\\Language\\" + cultureInfo.Name + ".xaml", UriKind.RelativeOrAbsolute);
+            try
+            {
+                return new ResourceDictionary() { Source = uri };
+            }
+            catch
+            {
+                return (ResourceDictionary)null;
+            }
+        }
+
+        public static string GetLang(string InfoKey)
+        {
+            try
+            {
+                return (string)Application.Current.TryFindResource((object)InfoKey) ?? InfoKey;
+            }
+            catch (Exception ex)
+            {
+                return InfoKey;
+            }
+        }
+
+        public static string GetLang(string InfoKey, string InfoValue)
+        {
+            try
+            {
+                return (string)Application.Current.TryFindResource((object)InfoKey) ?? InfoValue;
+            }
+            catch (Exception ex)
+            {
+                return InfoKey;
+            }
+        }
+
+        public static string GetLangType()
+        {
+            string lang = GetLang("Language");
+            return lang == "DefaultCH" ? "zh-cn" : (lang == "English" ? "en-us" : "zh-cn");
+        }
+    }
+}
