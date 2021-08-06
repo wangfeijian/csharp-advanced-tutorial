62ad13f52da45aca9feeae16058e1f45a15412f8
王飞箭 <wangfeijianhao@163.com>
2021-08-06 17:54:08 +0800

    纠正增加站位管理类出现的错误

--- AutoMationFramework/AutoMationFrameworkDll/StationManager.cs
+++ AutoMationFramework/AutoMationFrameworkDll/StationManager.cs
@@ -15,98 +15,183 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using AutoMationFrameworkSystemDll;
+using CommonTools.Tools;
 
 namespace AutoMationFrameworkDll
 {
     public class StationManager:SingletonPattern<StationManager>
     {
-        /// <summary>当前是否在自动运行状态</summary>
+        /// <summary>
+        /// 弹窗计数改变委托
+        /// </summary>
+        /// <param name="nCount"></param>
+        public delegate void ShowMessageCountChanged(int nCount);
+
+        /// <summary>
+        /// 定义一个站位状态变化委托函数
+        /// </summary>
+        /// <param name="OldState"></param>
+        /// <param name="NewState"></param>
+        public delegate void StateChangedHandler(StationState OldState, StationState NewState);
+
+        private static object _lockCount = new object();
+
+        /// <summary>
+        /// 站位链表
+        /// </summary>
+        public List<StationBase> _lsStation = new List<StationBase>();
+
+        /// <summary>
+        /// 站位集合
+        /// </summary>
+        public Dictionary<Control, StationBase> DicFormStation = new Dictionary<Control, StationBase>();
+
+        /// <summary>
+        /// 站位与自定义手动界面的集合
+        /// </summary>
+        public Dictionary<Control, StationBase> DicManualFormStation = new Dictionary<Control, StationBase>();
+
+        private StationState _nCurState = StationState.StateManual;
+
+        private int _showMessageCount = 0;
+        private bool _isAutoMode;
+
+        /// <summary>
+        /// 弹窗计数改变事件
+        /// </summary>
+        public event ShowMessageCountChanged OnShowMessageCountChanged;
+
+        /// <summary>
+        /// 当前是否全自动运行
+        /// </summary>
+        public bool BAutoMode
+        {
+            get
+            {
+                return _isAutoMode;
+            }
+            set
+            {
+                _isAutoMode = value;
+            }
+        }
+
+        /// <summary>
+        /// 站位当前状态
+        /// </summary>
+        public StationState CurrentState => _nCurState;
+
+        /// <summary>
+        /// 当前是否在自动运行状态
+        /// </summary>
         /// <returns></returns>
         public bool IsAutoRunning()
         {
-            return this.m_nCurState != StationState.STATE_MANUAL;
+            return _nCurState != StationState.StateManual;
         }
 
-        /// <summary>计数减一</summary>
+        /// <summary>
+        /// 计数减一
+        /// </summary>
         public void DecreaseOne()
         {
-            lock (StationMgr.m_lockCount)
+            lock (_lockCount)
             {
-                --this.m_nShowMessageCount;
-                // ISSUE: reference to a compiler-generated field
-                if (this.OnShowMessageCountChanged == null)
-                    return;
-                // ISSUE: reference to a compiler-generated field
-                this.OnShowMessageCountChanged(this.m_nShowMessageCount);
+                --_showMessageCount;
+                OnShowMessageCountChanged?.Invoke(_showMessageCount);
             }
         }
-        /// <summary>弹窗计数加一</summary>
+        /// <summary>
+        /// 弹窗计数加一
+        /// </summary>
         public void IncreaseOne()
         {
-            lock (StationMgr.m_lockCount)
+            lock (_lockCount)
             {
-                ++this.m_nShowMessageCount;
-                // ISSUE: reference to a compiler-generated field
-                if (this.OnShowMessageCountChanged == null)
-                    return;
-                // ISSUE: reference to a compiler-generated field
-                this.OnShowMessageCountChanged(this.m_nShowMessageCount);
+                ++_showMessageCount;
+               OnShowMessageCountChanged?.Invoke(_showMessageCount);
             }
         }
 
-        /// <summary>当前是否处于急停模式</summary>
+        /// <summary>
+        /// 当前是否处于急停模式
+        /// </summary>
         /// <returns></returns>
         public bool IsEmg()
         {
-            return this.m_nCurState == StationState.STATE_EMG;
+            return _nCurState == StationState.StateEmg;
         }
 
-        /// <summary>判断一个线程是在自动状态还是手动状态</summary>
+        /// <summary>
+        /// 判断一个线程是在自动状态还是手动状态
+        /// </summary>
         /// <param name="thread"></param>
         /// <returns></returns>
         public bool IsAutoThread(Thread thread)
         {
-            foreach (StationBase stationBase in this.m_lsStation)
+            foreach (StationBase stationBase in _lsStation)
             {
-                if (thread == stationBase.m_AutoThread)
+                if (thread == stationBase.AutoThread)
                     return true;
             }
             return false;
         }
 
-        /// <summary>判断一个线程是在自动状态还是手动状态</summary>
+        /// <summary>
+        /// 判断一个线程是在自动状态还是手动状态
+        /// </summary>
         /// <param name="thread"></param>
         /// <returns></returns>
         public bool IsManaualThread(Thread thread)
         {
-            foreach (StationBase stationBase in this.m_lsStation)
+            foreach (StationBase stationBase in _lsStation)
             {
-                if (thread == stationBase.m_ManualThread)
+                if (thread == stationBase.ManualThread)
                     return true;
             }
             return false;
         }
 
-        /// <summary>急停所有站位</summary>
+        /// <summary>
+        /// 急停所有站位
+        /// </summary>
         /// <returns></returns>
         public bool EmgStopAllStation()
         {
-            if (this.m_nCurState == StationState.STATE_EMG || this.m_nCurState == StationState.STATE_MANUAL)
+            if (_nCurState == StationState.StateEmg || _nCurState == StationState.StateManual)
                 return false;
-            if ((uint)SingletonTemplate<LanguageMgr>.GetInstance().LanguageID > 0U)
-                this.ShowLog("Alarm and emergency stop!!!", CommonTool.LogLevel.Error);
+            if (LocationServices.GetLangType() == "en-us")
+                ShowLog("Alarm and emergency stop!!!", LogLevel.Error);
             else
-                this.ShowLog("报警急停!!!", CommonTool.LogLevel.Error);
-            this.ChangeState(StationState.STATE_EMG);
-            foreach (StationBase stationBase in this.m_lsStation)
+                ShowLog("报警急停!!!", LogLevel.Error);
+            ChangeState(StationState.StateEmg);
+            foreach (StationBase stationBase in _lsStation)
             {
-                stationBase.SwitchState(StationState.STATE_EMG);
+                stationBase.SwitchState(StationState.StateEmg);
                 stationBase.EmgStop();
             }
-            foreach (StationBase stationBase in this.m_lsStation)
+            foreach (StationBase stationBase in _lsStation)
                 stationBase.StopManualRun();
             return true;
+        }
+
+        /// <summary>定义一个站位状态变化事件</summary>
+        public event StateChangedHandler StateChangedEvent;
+
+        /// <summary>
+        /// 站位切换状态同时触发状态变更事件
+        /// </summary>
+        /// <param name="newState"></param>
+        public void ChangeState(StationState newState)
+        {
+            if (_nCurState == newState)
+                return;
+            StationState nCurState = _nCurState;
+            _nCurState = newState;
+            StateChangedEvent?.Invoke(nCurState, _nCurState);
+            SingletonPattern<RunInforManager>.GetInstance().Info(nCurState + " change state to " + newState);
         }
     }
 }
