cd4e7dff4090f0d71f0f5a1b5c047ff0fd3a816e
王飞箭 <wangfeijianhao@163.com>
2022-02-17 18:01:15 +0800

    将基于Halcon的显示图片库完善

--- SosoVision/ImageDisplayTests/MainWindow.xaml.cs
+++ SosoVision/ImageDisplayTests/MainWindow.xaml.cs
@@ -25,7 +25,8 @@ namespace ImageDisplayTests
         public MainWindow()
         {
             InitializeComponent();
-            HOperatorSet.ReadImage(out testHObject,"D:/Desktop Image/1.jpg");
+            HOperatorSet.ReadImage(out testHObject, "D:/Desktop Image/1.jpg");
+            //HOperatorSet.ReadImage(out testHObject,"D:/Project/2021/08/彩色标签/20210830/Pic_2021_08_30_111241_blockId#8542.bmp");
             ShowImage.DisplayImage = testHObject;
         }
     }
--- SosoVision/SosoImageDisplay/ImageDisplayWindow.xaml.cs
+++ SosoVision/SosoImageDisplay/ImageDisplayWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -23,6 +24,16 @@ namespace ImageDisplay
         public static readonly DependencyProperty CameraColorProperty =
             DependencyProperty.Register("CameraColor", typeof(string), typeof(ImageDisplayWindow));
 
+        public string FillStyle
+        {
+            get { return (string)GetValue(FillStyleProperty); }
+            set { SetValue(FillStyleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CameraColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FillStyleProperty =
+            DependencyProperty.Register("FillStyle", typeof(string), typeof(ImageDisplayWindow));
+
         public HObject DisplayImage
         {
             get { return (HObject)GetValue(DisplayImageProperty); }
@@ -44,13 +55,23 @@ namespace ImageDisplay
             DependencyProperty.Register("DisplayRegion", typeof(HObject), typeof(ImageDisplayWindow));
 
         /// <summary>
+        /// 画出来的对象
+        /// </summary>
+        private List<HTuple> _drawObjects;
+
+        private object _lock = new object();
+
+        /// <summary>
         /// 加载Halcon图像选择工具
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void ShowImage_OnHInitWindow(object sender, EventArgs e)
         {
+            FillStyle = "fill";
+            _drawObjects = new List<HTuple>();
             ShowImage.HMoveContent = false;
+
             HWindow h = ShowImage.HalconWindow;
             if (h != null)
             {
@@ -91,6 +112,16 @@ namespace ImageDisplay
                 case "缩小":
                     ZoomButton_Click(ButtonNarrow, null);
                     break;
+                case "区域边框":
+                    RadioButtonMargin.IsChecked = !menu.IsChecked;
+                    RadioButtonFill.IsChecked = !RadioButtonMargin.IsChecked;
+                    RadioButtonFill_OnClick(null, null);
+                    break;
+                case "区域填充":
+                    RadioButtonFill.IsChecked = !menu.IsChecked;
+                    RadioButtonMargin.IsChecked = !RadioButtonFill.IsChecked;
+                    RadioButtonFill_OnClick(null, null);
+                    break;
             }
         }
 
@@ -122,6 +153,7 @@ namespace ImageDisplay
             Button temp = sender as Button;
             int scale = temp.ToolTip.ToString() == "放大" ? 10 : -10;
             ShowImage.HZoomWindowContents(ShowImage.ActualWidth / 2, ShowImage.ActualHeight / 2, scale);
+            ShowImageScale();
         }
 
         /// <summary>
@@ -132,6 +164,7 @@ namespace ImageDisplay
         private void FullShowImage(object sender, RoutedEventArgs e)
         {
             ShowImage.SetFullImagePart();
+            ShowImageScale();
         }
 
         /// <summary>
@@ -141,9 +174,14 @@ namespace ImageDisplay
         /// <param name="e"></param>
         private void ShowImage_OnHMouseMove(object sender, HSmartWindowControlWPF.HMouseEventArgsWPF e)
         {
+            if (DisplayImage == null)
+            {
+                return;
+            }
+
             HTuple homMat2D = new HTuple();
             HTuple imageX = new HTuple(), imageY = new HTuple();
-            HTuple showX = new HTuple { [0] = 0, [1] = 0, [2] = ShowImage.ActualHeight};
+            HTuple showX = new HTuple { [0] = 0, [1] = 0, [2] = ShowImage.ActualHeight };
             HTuple showY = new HTuple { [0] = 0, [1] = ShowImage.ActualWidth, [2] = 0 };
             HTuple partX = new HTuple
             {
@@ -160,9 +198,21 @@ namespace ImageDisplay
             HOperatorSet.VectorToHomMat2d(showX, showY, partX, partY, out homMat2D);
             HOperatorSet.AffineTransPoint2d(homMat2D, e.X, e.Y, out imageX, out imageY);
 
-            double posX = imageX.D + ShowImage.HImagePart.Left;
-            double posY = imageY.D + ShowImage.HImagePart.Top;
-            TextBlockPos.Text = $"X:{(int)posX},  Y:{(int)posY}";
+            int posX = (int)(imageX.D + ShowImage.HImagePart.Left);
+            int posY = (int)(imageY.D + ShowImage.HImagePart.Top);
+            TextBlockPos.Text = $"X:{posX},  Y:{posY}";
+            ShowImageScale();
+
+            HTuple channels = new HTuple();
+            HOperatorSet.CountChannels(DisplayImage, out channels);
+            if (channels[0].I == 1)
+            {
+                ShowImagePixel(posY, posX, false);
+            }
+            else
+            {
+                ShowImagePixel(posY, posX);
+            }
 
             homMat2D.Dispose();
             imageX.Dispose();
@@ -171,6 +221,236 @@ namespace ImageDisplay
             showY.Dispose();
             partX.Dispose();
             partY.Dispose();
+            channels.Dispose();
+        }
+
+        private void ShowImagePixel(int row, int col, bool isColor = true)
+        {
+            HTuple width = new HTuple();
+            HTuple height = new HTuple();
+            HOperatorSet.GetImageSize(DisplayImage, out width, out height);
+
+            if (row < 0 || row >= height || col < 0 || col >= width)
+            {
+                width.Dispose();
+                height.Dispose();
+                return;
+            }
+
+            if (isColor)
+            {
+                HObject rImg = new HObject();
+                HObject gImg = new HObject();
+                HObject bImg = new HObject();
+                HOperatorSet.Decompose3(DisplayImage, out rImg, out gImg, out bImg);
+
+                HTuple rVal = new HTuple();
+                HTuple gVal = new HTuple();
+                HTuple bVal = new HTuple();
+                HOperatorSet.GetGrayval(rImg, row, col, out rVal);
+                HOperatorSet.GetGrayval(gImg, row, col, out gVal);
+                HOperatorSet.GetGrayval(bImg, row, col, out bVal);
+
+                TextBlockPixel.Text = $"R: {rVal}  G： {gVal}  B: {bVal}";
+
+                rImg.Dispose();
+                gImg.Dispose();
+                bImg.Dispose();
+                rVal.Dispose();
+                gVal.Dispose();
+                bVal.Dispose();
+            }
+            else
+            {
+                HTuple grayVal = new HTuple();
+                HOperatorSet.GetGrayval(DisplayImage, row, col, out grayVal);
+
+                TextBlockPixel.Text = grayVal.ToString();
+
+                grayVal.Dispose();
+            }
+
+            width.Dispose();
+            height.Dispose();
+        }
+
+        private void ShowImageScale()
+        {
+            double scale = ShowImage.ActualWidth / ShowImage.HImagePart.Width * 100;
+            TextBlockScale.Text = $"缩放: {scale:f2} %";
+        }
+
+        private void ShowImage_OnHMouseWheel(object sender, HSmartWindowControlWPF.HMouseEventArgsWPF e)
+        {
+            ShowImageScale();
+        }
+
+
+        private void ButtonDrawLine_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button temp = sender as Button;
+            string index = temp.Tag.ToString();
+
+            DrawObjectToWindow(index);
+        }
+
+        /// <summary>
+        /// 向窗口画图形
+        /// </summary>
+        /// <param name="index">类型</param>
+        private void DrawObjectToWindow(string index)
+        {
+            HTuple draw = new HTuple();
+
+            switch (index)
+            {
+                case "1":
+                    HOperatorSet.CreateDrawingObjectLine(100, 100, 200, 200, out draw);
+                    HOperatorSet.SetDrawingObjectParams(draw, "line_width", 1);
+                    break;
+                case "2":
+                    HOperatorSet.CreateDrawingObjectRectangle2(200, 200, 0, 100, 100, out draw);
+                    break;
+                case "3":
+                    HOperatorSet.CreateDrawingObjectCircle(200, 200, 50, out draw);
+                    break;
+                case "4":
+                    HOperatorSet.CreateDrawingObjectEllipse(200, 200, 0, 100, 60, out draw);
+                    break;
+            }
+
+            HOperatorSet.AttachDrawingObjectToWindow(ShowImage.HalconWindow, draw);
+            _drawObjects.Add(draw);
+        }
+
+        private void MenuItemDraw_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem temp = sender as MenuItem;
+
+            string index = temp.Tag.ToString();
+
+            DrawObjectToWindow(index);
+        }
+
+        /// <summary>
+        /// 将绘制的图形转换成区域
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonEdit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button temp = sender as Button;
+            string index = temp.Tag.ToString();
+            CollectionEdit(index);
+        }
+
+        private void CollectionEdit(string index)
+        {
+            HTuple param;
+            HObject region;
+            HOperatorSet.GenEmptyObj(out region);
+
+            if (_drawObjects.Count > 0)
+            {
+                foreach (var drawingObject in _drawObjects)
+                {
+                    HObject unionRegion;
+                    HOperatorSet.GenEmptyObj(out unionRegion);
+                    HObject tempRegion;
+                    HOperatorSet.GetDrawingObjectIconic(out tempRegion, drawingObject);
+                    HOperatorSet.GetDrawingObjectParams(drawingObject, "line_width", out param);
+                    if (param == 1)
+                    {
+                        region = tempRegion;
+                        DisplayRegion = region;
+                        param.Dispose();
+                        ClearAllObjects();
+                        return;
+                    }
+
+                    if (index == "0" || index == "2" || index == "3")
+                    {
+                        HOperatorSet.Union2(region, tempRegion, out unionRegion);
+                    }
+                    else
+                    {
+                        HObject equalRegion;
+                        HTuple isEqual;
+                        HOperatorSet.GenEmptyObj(out equalRegion);
+                        HOperatorSet.TestEqualObj(equalRegion, region, out isEqual);
+
+                        if (isEqual)
+                        {
+                            HOperatorSet.Union2(region, tempRegion, out unionRegion);
+                        }
+                        else
+                        {
+                            HOperatorSet.Intersection(region, tempRegion, out unionRegion);
+                        }
+
+                    }
+
+                    region = unionRegion;
+                    DisplayRegion = region;
+                }
+
+                if (index == "3")
+                {
+                    HObject interRegion;
+                    HOperatorSet.Complement(DisplayRegion, out interRegion);
+                    DisplayRegion = interRegion;
+                }
+            }
+
+            HOperatorSet.Connection(DisplayRegion, out region);
+            DisplayRegion = region;
+            ClearAllObjects();
+        }
+
+        private void ClearAllObjects()
+        {
+            lock (_lock)
+            {
+                foreach (HTuple dobj in _drawObjects)
+                {
+                    HOperatorSet.ClearDrawingObject(dobj);
+                }
+                _drawObjects.Clear();
+            }
+        }
+
+        private void RadioButtonFill_OnClick(object sender, RoutedEventArgs e)
+        {
+            FillStyle = RadioButtonFill.IsChecked == true ? "fill" : "margin";
+        }
+
+        private void CheckBoxDrawEnable_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItemDraw.IsEnabled = CheckBoxDrawEnable.IsChecked == true;
+            MenuItemEdit.IsEnabled = CheckBoxDrawEnable.IsChecked == true;
+            MenuItemDrawFill.IsEnabled = CheckBoxDrawEnable.IsChecked == true;
+        }
+
+        private void MenuItemEditRoi_OnClick(object sender, RoutedEventArgs e)
+        {
+            CheckBoxDrawEnable.IsChecked = !CheckBoxDrawEnable.IsChecked;
+            CheckBoxDrawEnable_OnClick(null, null);
+        }
+
+        private void MenuItemCollection_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem temp = sender as MenuItem;
+            string index = temp.Tag.ToString();
+            CollectionEdit(index);
+        }
+
+        private void ButtonClearRegion_OnClick(object sender, RoutedEventArgs e)
+        {
+            HObject tempRegion;
+            HOperatorSet.GenEmptyObj(out tempRegion);
+            DisplayRegion = tempRegion;
+
+            tempRegion.Dispose();
         }
     }
 }
