aad621264cf38725b8a7ebf24b83a6870fad5c85
王飞箭 <wangfeijianhao@163.com>
2022-04-07 11:43:21 +0800

    增加.net 6中C# 支持的新语法demo

---
+++ NewGrammerTest/NewGrammerTest/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace NewGrammerTest
+{
+    public record Person(string Name, int Id);
+
+    public record Student : Person, IDisposable
+    {
+        public Student(string Name, int Id) : base(Name, Id)
+        {
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"开始清除对象 {Name}");
+        }
+    }
+}
---
+++ NewGrammerTest/NewGrammerTest/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Hello, World!");
+Person p = new Person("wang", 123);
+Console.WriteLine(p);
+using Student s1 = new Student("soso1", 1),
+              s2 = new Student("soso2", 2),
+              s3 = new Student("soso3", 3);
+
+Console.WriteLine(s1);
+
+Console.WriteLine(s2);
+Console.WriteLine(s3);
