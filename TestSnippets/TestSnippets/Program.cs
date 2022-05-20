d9b179f7d3a80d5a627a0cda0edddc7e0f533fab
王飞箭 <wangfeijianhao@163.com>
2022-05-20 10:16:39 +0800

    增加Vision Pro中仿射的使用方法

--- TestSnippets/TestSnippets/Program.cs
+++ TestSnippets/TestSnippets/Program.cs
@@ -1,4 +1,6 @@
-﻿using TestSnippets;
+﻿
+using System.Reflection;
+using TestSnippets;
 
 //使用XmlDocument操作
 //XmlReadWrite.CreateXmlFile("d:/test.xml", "Test");
@@ -8,6 +10,112 @@
 
 //使用XDocument操作
 //XmlReadWriteUseXDocument.CreateXmlFile("d:/testX.xml", "TestXml");
-//XmlReadWriteUseXDocument.AddXmlNodeAndSetAttribute("d:/testX.xml", "TestXml", "TestParm","Param");
-XmlReadWriteUseXDocument.GetXmlNodeAttributeValue("d:/testX.xml", "Param", "name");
-XmlReadWriteUseXDocument.GetXmlNodeAttributeValue("d:/testX.xml", "Param", "version");
+//XmlReadWriteUseXDocument.AddXmlNodeAndSetAttribute("d:/testX.xml", "TestXml", "TestParm", "Param");
+//XmlReadWriteUseXDocument.GetXmlNodeAttributeValue("d:/testX.xml", "Param", "name");
+//XmlReadWriteUseXDocument.GetXmlNodeAttributeValue("d:/testX.xml", "Param", "version");
+
+#region 监视文件
+//using var watcher = new FileSystemWatcher(@"D:\TestFileChange");
+
+//watcher.NotifyFilter = NotifyFilters.Attributes
+//                     | NotifyFilters.CreationTime
+//                     | NotifyFilters.DirectoryName
+//                     | NotifyFilters.FileName
+//                     | NotifyFilters.LastAccess
+//                     | NotifyFilters.LastWrite
+//                     | NotifyFilters.Security
+//                     | NotifyFilters.Size;
+
+//watcher.Changed += OnChanged;
+//watcher.Created += OnCreated;
+//watcher.Deleted += OnDeleted;
+//watcher.Renamed += OnRenamed;
+//watcher.Error += OnError;
+
+//watcher.Filter = "*.txt";
+//watcher.IncludeSubdirectories = true;
+//watcher.EnableRaisingEvents = true;
+
+//Console.WriteLine("Press enter to exit.");
+//Console.ReadLine();
+
+//static void OnChanged(object sender, FileSystemEventArgs e)
+//{
+//    if (e.ChangeType != WatcherChangeTypes.Changed)
+//    {
+//        return;
+//    }
+//    Console.WriteLine($"Changed: {e.FullPath}");
+//}
+
+//static void OnCreated(object sender, FileSystemEventArgs e)
+//{
+//    string value = $"Created: {e.FullPath}";
+//    Console.WriteLine(value);
+//}
+
+//static void OnDeleted(object sender, FileSystemEventArgs e) =>
+//    Console.WriteLine($"Deleted: {e.FullPath}");
+
+//static void OnRenamed(object sender, RenamedEventArgs e)
+//{
+//    Console.WriteLine($"Renamed:");
+//    Console.WriteLine($"    Old: {e.OldFullPath}");
+//    Console.WriteLine($"    New: {e.FullPath}");
+//}
+
+//static void OnError(object sender, ErrorEventArgs e) =>
+//    PrintException(e.GetException());
+
+//static void PrintException(Exception? ex)
+//{
+//    if (ex != null)
+//    {
+//        Console.WriteLine($"Message: {ex.Message}");
+//        Console.WriteLine("Stacktrace:");
+//        Console.WriteLine(ex.StackTrace);
+//        Console.WriteLine();
+//        PrintException(ex.InnerException);
+//    }
+//}
+#endregion
+
+#region 遍历枚举
+
+//foreach (var item in Enum.GetValues(typeof(EnumTest)))
+//{
+//    Console.WriteLine($"{(int)item}--{item}");
+//}
+//enum EnumTest
+//{
+//    First,
+//    Second,
+//    Third,
+//    Fourth
+//}
+#endregion
+
+
+#region 反射
+
+TestClass test = new TestClass();
+Type t = test.GetType();
+var methods = t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.DeclaredOnly);
+foreach (var method in methods)
+{
+    Console.WriteLine(method.Name);
+    method.Invoke(test, null);
+}
+class TestClass
+{
+    public void Test1()
+    {
+        Console.WriteLine("Test1 Invoke");
+    }
+
+    public void Test2()
+    {
+        Console.WriteLine("Test2 Invoke");
+    }
+}
+#endregion
---
+++ TestVisionTransform/TestVisionTransform/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cognex.VisionPro;
+
+namespace TestVisionTransform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double outX, outY;
+            CogTransform2DLinear cogTransform2D = new CogTransform2DLinear();
+            // 这里面的TranslationX和TranslationY其实就是吸嘴的旋转中心
+            // 平移的offset也是这两个，在Halcon相当于下面的代码
+            // hom_mat2d_identity (HomMat2DIdentity)
+            // hom_mat2d_translate (HomMat2DIdentity, 30, 20, HomMat2DTranslate)
+            // hom_mat2d_rotate (HomMat2DTranslate, rad(90), 30, 20, HomMat2DRotate)
+
+            cogTransform2D.TranslationX = 30;
+            cogTransform2D.TranslationY = 20;
+            cogTransform2D.Rotation = CogMisc.DegToRad(90);
+
+            // 这里仿射的点，只是基于刚刚的旋转中心，先旋转再平移的点
+            // 所以在实际运用中，需要减去目标位置得到偏移量
+            cogTransform2D.MapPoint(5, 5, out outX, out outY);
+        }
+    }
+}
