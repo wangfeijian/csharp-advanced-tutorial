03686e4dc48cdc31779b4f5f6082e9f668eca663
王飞箭 <wangfeijianhao@163.com>
2022-05-20 14:55:59 +0800

    Vision pro中基于旋转的仿射变换，增加原理说明

--- TestVisionTransform/TestVisionTransform/Program.cs
+++ TestVisionTransform/TestVisionTransform/Program.cs
@@ -12,20 +12,22 @@ namespace TestVisionTransform
         static void Main(string[] args)
         {
             double outX, outY;
+            double rotateX = 130, rotateY = 20, inX = 15, inY = 25, deg = 14.3;
             CogTransform2DLinear cogTransform2D = new CogTransform2DLinear();
-            // 这里面的TranslationX和TranslationY其实就是吸嘴的旋转中心
-            // 平移的offset也是这两个，在Halcon相当于下面的代码
+            // 这里面的TranslationX和TranslationY其实就是将坐标原点移动到旋转中心
+            // 移动之后，再通过旋转的角度，将点位旋转到指定的位置
             // hom_mat2d_identity (HomMat2DIdentity)
-            // hom_mat2d_translate (HomMat2DIdentity, 30, 20, HomMat2DTranslate)
-            // hom_mat2d_rotate (HomMat2DTranslate, rad(90), 30, 20, HomMat2DRotate)
+            // hom_mat2d_rotate (HomMat2DIdentity, rad(deg), rotateX, rotateY, HomMat2DRotate)
 
-            cogTransform2D.TranslationX = 30;
-            cogTransform2D.TranslationY = 20;
-            cogTransform2D.Rotation = CogMisc.DegToRad(90);
+            cogTransform2D.TranslationX = rotateX;
+            cogTransform2D.TranslationY = rotateY;
+            cogTransform2D.Rotation = CogMisc.DegToRad(deg);
 
-            // 这里仿射的点，只是基于刚刚的旋转中心，先旋转再平移的点
-            // 所以在实际运用中，需要减去目标位置得到偏移量
-            cogTransform2D.MapPoint(5, 5, out outX, out outY);
+            // 这里仿射出来的值都是经过平移后得到的，所以在映射之前要将输入点减去之前的平移量，才能得到旋转后正确的点位
+            // 所以在实际运用中，需要用仿射的实际点位，减去上面平移的偏移量
+            cogTransform2D.MapPoint(inX - rotateX, inY - rotateY, out outX, out outY);
+            Console.WriteLine($"outX: {outX:f3} __  outY: {outY:f3}");
+            Console.ReadKey();
         }
     }
 }
